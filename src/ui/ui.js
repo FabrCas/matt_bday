7332174313757fe19f1e8dd5ec7f992c0a5a3f99
FabrCas <fabriziocasadei97@gmail.com>
2026-08-03 01:21:25 +0200

    changed config

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -65,7 +65,7 @@ export function updateGameOver({ coins, distance, amount, message}) {
   els.goDistance.textContent = Math.floor(distance);
   els.wonAmount.textContent = formatMoney(amount);
   els.endMessage.textContent = message;
-  els.btnRetry.classList.toggle("hidden", !CONFIG.debug && !CONFIG.game.allowReplay);
+  els.btnRetry.classList.toggle("hidden", !CONFIG.game.allowReplay);
 }
 export function updateWishes() {
   els.wishesMessage.textContent = CONFIG.game.wishesMessage;
